005384bee1554361f69c865d2812cfab00dafeb6
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-17 14:54:39 +0100

    Exercice completé

---
+++ Variables - Exercice 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables___Exercice_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nom, prenom;
+
+            Console.WriteLine("Quel est votre nom ?");
+            nom = Console.ReadLine();
+            Console.WriteLine("Quel est votre prénom ?");
+            prenom = Console.ReadLine();
+            Console.WriteLine("Bonjour " + nom + " " + prenom + " nous sommes le " + DateTime.Now.ToLongDateString() + ". Comment allez-vous ?");
+            Console.ReadLine();
+        }
+    }
+}
